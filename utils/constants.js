55fcfd0841bfc0705098b4c936be7b2a5dd4f847
godninoff <godninoff21@yandex.ru>
2022-03-09 15:07:21 +0500

    switch mongo to atlas

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,8 @@
 const {
   NODE_ENV,
   JWT_SECRET,
-  MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb',
+  MONGO_URL = 'mongodb+srv://godninoff:214972Ag@cluster0.oo43b.mongodb.net/savedMovies',
+  // 'mongodb://localhost:27017/bitfilmsdb'
   PORT = process.env.PORT || 3000,
 } = process.env;
 
